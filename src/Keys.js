7ed31a64ed3d829df0ebb03f03be21619a372d68
Desmond Harris <desmondharris912@gmail.com>
2018-11-27 12:12:29 -0600

    Add files via upload

---
+++ src/Keys.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Keys extends Component {
+  render() {
+    return <div className="Keys"> {this.props.children} </div>;
+  }
+}
+
+export default Keys;
